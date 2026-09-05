245b7c55478fc97ebf3664f9d8af10d5c84c6cd8
Kayra <kayra19206@gmail.com>
2026-09-05 22:21:02 +1000

    CH3 L4: Transient Queues

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/KayraBulbul/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/KayraBulbul/learn-pub-sub-starter/internal/pubsub"
+	"github.com/KayraBulbul/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+	connectionString := "amqp://guest:guest@localhost:5672/"
+
+	connection, err := amqp.Dial(connectionString)
+	if err != nil {
+		log.Fatal("error making connection")
+	}
+	defer connection.Close()
+
+	fmt.Println("Peril server connected!")
+	str, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatal("error welcoming client")
+	}
+
+	queueName := fmt.Sprintf("pause.%s", str)
+	_, _, err = pubsub.DeclareAndBind(connection, routing.ExchangePerilDirect, queueName, routing.PauseKey, pubsub.Transient)
+	if err != nil {
+		log.Fatal("error declare and bind")
+	}
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("Shutting down connection...")
 }
---
+++ internal/pubsub/declare_and_bind.go
@@ -0,0 +1,38 @@
+package pubsub
+
+import amqp "github.com/rabbitmq/amqp091-go"
+
+type SimpleQueueType int
+
+const (
+	Durable SimpleQueueType = iota
+	Transient
+)
+
+func DeclareAndBind(conn *amqp.Connection, exchange, queueName, key string, queueType SimpleQueueType) (*amqp.Channel, amqp.Queue, error) {
+	channel, err := conn.Channel()
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	isDurable := queueType == Durable
+
+	queue, err := channel.QueueDeclare(
+		queueName,
+		isDurable,
+		!isDurable,
+		!isDurable,
+		false,
+		nil,
+	)
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+	
+	err = channel.QueueBind(queueName, key, exchange, false, nil)
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	return channel, queue, nil
+}
